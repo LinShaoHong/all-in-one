cda79c02510cbdc21e9d7f59f2923bb35e61faed
蔺绍宏 <shaohonglin0411@gmail.com>
2019-07-11 20:42:34 +0800

    test

--- backend/spider/src/main/java/com/github/sun/spider/App.java
+++ backend/spider/src/main/java/com/github/sun/spider/App.java
@@ -4,10 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
-/**
- * @Author LinSH
- * @Date: 12:35 PM 2019-07-11
- */
 @SpringBootApplication
 @ComponentScan({"com.github.sun"})
 public class App {
